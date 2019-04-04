74bf126d8ff91a80f4e481f76978a25cd40d2a7b
User-1 <user-1@MacBook-28.local>
2019-04-03 18:41:42 -0600

    working on pin layouts and media queiries

--- src/Components/Pins/Pins.js
+++ src/Components/Pins/Pins.js
@@ -1,6 +1,8 @@
-import React, {Component} from 'react'
+import React, {Component} from 'react';
+import InfiniteScroll from 'react-infinite-scroll-component';
 import axios from 'axios';
 import { app_id } from '../../config.js';
+import loader from '../../media/loader_gif.gif'
 import './Pins.css';
 
 
@@ -11,23 +13,36 @@ class Pins extends Component{
         super();
         this.state = {
             response: [],
-            image: '',
+            images: [],
+            loaded: false
         }
+
+        this.fetchImages = this.fetchImages.bind(this);
     }
 
     componentDidMount(){
-        axios.get( 'https://api.unsplash.com/photos?page=2&per_page=30&client_id=' + app_id ).then((res) => {
+        axios.get( `https://api.unsplash.com/photos?page=2&per_page=20&client_id=${ app_id }`).then((res) => {
             console.log(res.data);
             this.setState({
                 response: res.data,
                 image: res.data[0].urls.small
             })
-            console.log(this.state.response[0].urls.small)
         })
+ }
+
+    fetchImages(count){
+        count = 10;
+        axios.get(`https://api.unsplash.com/photos/random?client_id=${ app_id }&count=${ count }`)
+        .then(res => {
+            this.setState({
+                images: [...this.state.images, ...res.data],
+                loaded: true
+            })})
     }
 
     render(){
         let arr = this.state.response;
+        let loading = this.state.images;
 
         return(
             <div id='pins'>
@@ -42,7 +57,25 @@ class Pins extends Component{
                         </div>
                     </div>
                 })}
-                
+                <InfiniteScroll
+                    dataLength={this.state.images}
+                    next={() => this.fetchImages(1)}
+                    hasMore={true}
+                    loader={
+                        <img src={ loader } alt='loading' style={{height: '120px'}}/>
+                    }
+                >
+                    {loading.map( (img, id) => {
+                        return  <div key = { id } id='pin-container'>
+                        {/* img is already the object on the array */}
+                            <img id="img" src={img.urls.small} />
+                            <div id='desc-box'>
+                                <p>{img.alt_description} by: {img.user.first_name}</p>
+                                <div id='three-dots-option'> ... </div>
+                            </div>
+                        </div>
+                        })}
+                </InfiniteScroll>
             </div>
         )
     }
